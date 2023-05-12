61ede3c8d99070f2c4683ad6bfbe9315142ecf88
tkohei57 <con.kt.57@gmail.com>
2023-05-12 17:50:09 +0900

    ユーザー管理機能の修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]]+\z/, message: 'is invalid. Input full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :last_name
     validates :first_name
   end
