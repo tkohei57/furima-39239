b1f11869ca2f6d9d7d09d81feaf7f4684672b2d9
tkohei57 <con.kt.57@gmail.com>
2023-05-22 14:07:42 +0900

    商品購入機能の実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,49 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
+  def index
+    @item = Item.find(params[:item_id])
+    @order_shipping = OrderShipping.new
+    if @item.order == nil
+      if current_user.id == @item.user_id
+        to_root
+      end
+    else
+      to_root
+    end
+  end
+
+  def create
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      pay_item
+      @order_shipping.save
+      to_root
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping).permit(:post_code, :prefecture_id, :municipalities, :address, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    item = Item.find(order_params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def to_root
+    redirect_to root_path
+  end
+end
